6e8eae8dc3647bccb4a888a2a65e5eeb3a46e59e
CSL-IBM <changsup.lim1@ibm.com>
2024-07-25 18:31:22 +0900

    Update 💵TTW_AskAR.py
    직접 추가 버전

--- 💵TTW_AskAR.py
+++ 💵TTW_AskAR.py
@@ -87,6 +87,11 @@ with col2:
     profile = Image.open("images/profile.png")
     st.image(profile, width=280)
 
+endorsements = {
+    "img1": "https://user-images.githubusercontent.com/90204593/238169843-12872392-f2f1-40a6-a353-c06a2fa602c5.png",
+    "img2": "https://user-images.githubusercontent.com/90204593/238171251-5f4c5597-84d4-4b4b-803c-afe74e739070.png",
+    "img3": "https://user-images.githubusercontent.com/90204593/238171242-53f7ceb3-1a71-4726-a7f5-67721419fef8.png"
+}
        
 st.write("---")
 with st.container():  
