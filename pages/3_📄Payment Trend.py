7294b06010ac68de3ce4c95c772d867e6cbf1046
CSL-IBM <changsup.lim1@ibm.com>
2024-07-25 17:20:22 +0900

    Update 3_📄Payment Trend.py

--- pages/3_📄Payment Trend.py
+++ pages/3_📄Payment Trend.py
@@ -25,14 +25,8 @@ def main():
 
     # Example inquiries section
     example_inquiries = [
-        "DueDate > DATE('now')",
-        "Collector = 'Lisa' AND Category = 'Yellow'",
-        "Collector = 'David' AND ForecastCode = 'AUTO'",
-        "Collector = 'John' AND ForecastDate > '2024-08-01'",
-        "ForecastCode = 'AUTO' GROUP BY Collector",
-        "DueDate > '2024-08-10'",
-        "Category = 'Green' GROUP BY Collector"
-    ]
+        "CustomerNumber = '843937'",
+]
     
     st.markdown("**Example Inquiries:**")
     selected_inquiry = st.selectbox("Select an inquiry example:", example_inquiries)
