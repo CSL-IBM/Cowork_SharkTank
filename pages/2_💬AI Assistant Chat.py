03b6f816719a1945516de715e5a8e8d64b4c40a5
CSL-IBM <changsup.lim1@ibm.com>
2024-07-25 15:15:16 +0900

    Update 2_💬AI Assistant Chat.py

--- pages/2_💬AI Assistant Chat.py
+++ pages/2_💬AI Assistant Chat.py
@@ -25,7 +25,7 @@ def create_table_from_csv():
                  )''')
 
     # Read data from CSV and insert into SQLite table
-    with open('transactions.csv', 'r', newline='', encoding='utf-8') as csvfile:
+    with open('transactions_EnageAR&Contract.csv', 'r', newline='', encoding='utf-8') as csvfile:
         csvreader = csv.reader(csvfile)
         next(csvreader)  # Skip header
         for row in csvreader:
