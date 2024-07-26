eab7f58751d158553bfe14a79e319f6d9ac6aa9f
CSL-IBM <changsup.lim1@ibm.com>
2024-07-26 13:35:36 +0900

    Update 💵TTW_AskAR.py
    endorsement 하단 gif 수정중

--- 💵TTW_AskAR.py
+++ 💵TTW_AskAR.py
@@ -88,8 +88,8 @@ with col2:
     st.image(profile, width=280)
 
 endorsements = {
-    "img1": "https://user-images.githubusercontent.com/90204593/238169843-12872392-f2f1-40a6-a353-c06a2fa602c5.png",
-    "img2": "https://user-images.githubusercontent.com/90204593/238171251-5f4c5597-84d4-4b4b-803c-afe74e739070.png",
+    "img1": "https://raw.githubusercontent.com/CSL-IBM/Cowork_SharkTank/main/images/git_background_1.png",
+    "img2": "https://raw.githubusercontent.com/CSL-IBM/Cowork_SharkTank/main/images/git_background_2.png",
     "img3": "https://user-images.githubusercontent.com/90204593/238171242-53f7ceb3-1a71-4726-a7f5-67721419fef8.png"
 }
        
