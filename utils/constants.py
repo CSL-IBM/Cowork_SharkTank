ae86de8258785956cccf90243d3ea671e59768f7
CSL-IBM <changsup.lim1@ibm.com>
2024-07-30 14:07:20 +0900

    Update constants.py
    다시 <b>로 변경

--- utils/constants.py
+++ utils/constants.py
@@ -40,9 +40,9 @@ info = {
    "Full_Name":"TTS_AskAR", 
    "Intro": "Text-To-SQL : Korea AR Team_🦈SharkTank Challenge",
    "About": """Hi User, welcome to TTS(Text-To-SQL).<br>
-    I'm here to assist you with inquiries regarding **EngageAR, Contract Information, and Payment Trend**.  
-    You can get started by selecting the button below. If you want more information about our team, click the **💵Korea ART link**.  
-    Additionally, if you have any ideas or suggestions about this TTS, feel free to reach out to us using the **📨Contact Me**.  
+    I'm here to assist you with inquiries regarding <b>EngageAR, Contract Information, and Payment Trend</b>.<br>
+    You can get started by selecting the button below. If you want more information about our team, click the <b>💵Korea ART link</b>.<br>
+    Additionally, if you have any ideas or suggestions about this TTS, feel free to reach out to us using the <b>📨Contact Me</b>.<br>
     Thank you!☺️""",
    "Project":"*",
    "Medium":"*",
