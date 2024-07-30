ff4fb302f5b72e9749c2dcd317be5d099c716a93
CSL-IBM <changsup.lim1@ibm.com>
2024-07-30 11:23:36 +0900

    Update 1_❓AskEngageAR.py
    원복

--- pages/1_❓AskEngageAR.py
+++ pages/1_❓AskEngageAR.py
@@ -132,9 +132,8 @@ def main():
         try:
             transactions = fetch_transactions(sql_condition)
             transactions.index = transactions.index + 1  # Change index to start from 1
-            st.markdown(f"**Filtered Transactions: {total_lines} lines**")  # Display the total number of lines
+            st.markdown("**Filtered Transactions:**")
             st.dataframe(transactions)
-            total_lines = len(transactions)  # Get the total number of lines
         except Exception as e:
             st.markdown(f"**Error occurred:** {str(e)}")
 
