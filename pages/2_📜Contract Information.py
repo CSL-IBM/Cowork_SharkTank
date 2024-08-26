10aa97cc24c40649239b81f5fae4de9027e6dd83
CSL-IBM <changsup.lim1@ibm.com>
2024-08-26 17:58:28 +0900

    Update 2_📜Contract Information.py
    위치수정

--- pages/2_📜Contract Information.py
+++ pages/2_📜Contract Information.py
@@ -118,17 +118,19 @@ def main():
             st.markdown(f"**Filtered Transactions: {total_lines} {line_text}**")  # Display the total number of lines
             st.dataframe(transactions)
 
-            # Download button
-            csv_data = transactions.to_csv(index=False)
-            st.download_button(
-                label="Download Filtered Data as CSV",
-                data=csv_data,
-                file_name='filtered_transactions.csv',
-                mime='text/csv'
             )
             
         except Exception as e:
             st.markdown(f"**Error occurred:** {str(e)}")
+
+    # Add download button for CSV file
+    with open('transactions_EngageAR_Contract.csv', 'r') as file:
+        st.download_button(
+            label="Download Raw Data",
+            data=file,
+            file_name='transactions_EngageAR_Contract.csv',
+            mime='text/csv'
+        )
 
 if __name__ == '__main__':
     main()
