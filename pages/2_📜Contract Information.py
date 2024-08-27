26255bed570862bb89e67894f813691f7ae208ef
CSL-IBM <changsup.lim1@ibm.com>
2024-08-27 11:20:00 +0900

    Update 2_📜Contract Information.py
    버튼 수정중

--- pages/2_📜Contract Information.py
+++ pages/2_📜Contract Information.py
@@ -83,14 +83,7 @@ def main():
 
     # Form for inquiry submission
     inquiry = st.text_input('Submit an Inquiry:', selected_inquiry)
-    # Add download button for CSV file
-        with open('transactions_EngageAR_Contract.csv', 'r') as file:
-            st.download_button(
-            label="Download Raw Data",
-            data=file,
-            file_name='transactions_EngageAR_Contract.csv',
-            mime='text/csv'
-                
+
     # Display transactions table based on the inquiry
     if st.button('Submit'):
         try:
@@ -125,10 +118,17 @@ def main():
             st.markdown(f"**Filtered Transactions: {total_lines} {line_text}**")  # Display the total number of lines
             st.dataframe(transactions)
 
-                            )
-            
         except Exception as e:
             st.markdown(f"**Error occurred:** {str(e)}")
+
+    # Display the download button outside of the submit button logic
+    with open('transactions_EngageAR_Contract.csv', 'r') as file:
+        st.download_button(
+            label="Download Raw Data",
+            data=file,
+            file_name='transactions_EngageAR_Contract.csv',
+            mime='text/csv'
+        )
 
 if __name__ == '__main__':
     main()
